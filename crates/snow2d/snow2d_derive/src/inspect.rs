642fd4e2a3d7e1f4f01c3989305b64c570f330f1
toyboot4e <toyboot4e@gmail.com>
2021-05-04 16:21:50 +0900

    extend where bounds in `Inspect` impls

--- crates/snow2d/snow2d_derive/src/inspect.rs
+++ crates/snow2d/snow2d_derive/src/inspect.rs
@@ -8,19 +8,30 @@ pub fn impl_inspect(ast: syn::DeriveInput) -> TokenStream2 {
 
     match args.data {
         ast::Data::Struct(ref fields) => self::inspect_struct(&args, fields),
-        ast::Data::Enum(ref fields) => self::inspec_unit_enum(&args, fields),
+        ast::Data::Enum(ref fields) => self::inspect_unit_enum(&args, fields),
     }
 }
 
+fn create_impl_generics(args: &args::TypeArgs) -> Generics {
+    let mut generics = args.generics.clone();
+
+    generics.make_where_clause().predicates.extend(
+        args.all_fields()
+            .iter()
+            .filter(|f| !f.skip)
+            .map(|f| &f.ty)
+            .map::<WherePredicate, _>(|ty| parse_quote! { #ty: Inspect }),
+    );
+
+    generics
+}
+
 /// Fill the `inspect` function body to derive `Inspect`
-fn generate_inspect_impl(
-    ty_name: &Ident,
-    generics: &Generics,
-    inspect_body: TokenStream2,
-) -> TokenStream2 {
+fn generate_inspect_impl(args: &args::TypeArgs, inspect_body: TokenStream2) -> TokenStream2 {
+    let generics = self::create_impl_generics(args);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    // TODO: add where clause
+    let ty_name = &args.ident;
 
     quote! {
         impl #impl_generics snow2d::utils::Inspect for #ty_name #ty_generics #where_clause
@@ -28,53 +39,6 @@ fn generate_inspect_impl(
             fn inspect(&mut self, ui: &imgui::Ui, label: &str) {
                 #inspect_body
             }
-        }
-    }
-}
-
-fn inspect_struct(args: &args::TypeArgs, fields: &ast::Fields<args::FieldArgs>) -> TokenStream2 {
-    if let Some(as_) = args.as_.as_ref() {
-        let as_: Type = parse_str(as_).unwrap();
-
-        self::generate_inspect_impl(
-            &args.ident,
-            &args.generics,
-            quote! {
-                let mut x: #as_ = (*self).into();
-                x.inspect(ui, label);
-                *self = x.into();
-            },
-        )
-    } else {
-        let is_newtype = fields.style == ast::Style::Tuple && fields.len() == 1;
-        if is_newtype {
-            // delegate the inspection to the only field
-            self::generate_inspect_impl(
-                &args.ident,
-                &args.generics,
-                quote! {
-                    self.0.inspect(ui, label);
-                },
-            )
-        } else {
-            // inspect each field
-            let field_inspectors = self::collect_field_inspectors(&fields);
-
-            self::generate_inspect_impl(
-                &args.ident,
-                &args.generics,
-                quote! {
-                    imgui::TreeNode::new(&imgui::im_str!("{}", label))
-                        .flags(
-                            imgui::TreeNodeFlags::OPEN_ON_ARROW |
-                            imgui::TreeNodeFlags::OPEN_ON_DOUBLE_CLICK
-                        )
-                        .build(ui, ||
-                    {
-                        #(#field_inspectors)*
-                    })
-                },
-            )
         }
     }
 }
@@ -119,7 +83,44 @@ fn collect_field_inspectors<'a>(
         })
 }
 
-fn inspec_unit_enum(args: &args::TypeArgs, variants: &[args::VariantArgs]) -> TokenStream2 {
+fn inspect_struct(args: &args::TypeArgs, fields: &ast::Fields<args::FieldArgs>) -> TokenStream2 {
+    let inspect = if let Some(as_) = args.as_.as_ref() {
+        let as_: Type = parse_str(as_).unwrap();
+
+        quote! {
+            let mut x: #as_ = (*self).into();
+            x.inspect(ui, label);
+            *self = x.into();
+        }
+    } else {
+        let is_newtype = fields.style == ast::Style::Tuple && fields.len() == 1;
+        if is_newtype {
+            // delegate the inspection to the only field
+            quote! {
+                self.0.inspect(ui, label);
+            }
+        } else {
+            // inspect each field
+            let field_inspectors = self::collect_field_inspectors(&fields);
+
+            quote! {
+                imgui::TreeNode::new(&imgui::im_str!("{}", label))
+                    .flags(
+                        imgui::TreeNodeFlags::OPEN_ON_ARROW |
+                        imgui::TreeNodeFlags::OPEN_ON_DOUBLE_CLICK
+                    )
+                    .build(ui, ||
+                {
+                    #(#field_inspectors)*
+                })
+            }
+        }
+    };
+
+    self::generate_inspect_impl(args, inspect)
+}
+
+fn inspect_unit_enum(args: &args::TypeArgs, variants: &[args::VariantArgs]) -> TokenStream2 {
     for v in variants {
         assert!(
             v.fields.is_empty(),
@@ -139,8 +140,7 @@ fn inspec_unit_enum(args: &args::TypeArgs, variants: &[args::VariantArgs]) -> To
     let variant_names = variants.iter().map(|v| format!("{}", v.ident));
 
     self::generate_inspect_impl(
-        &args.ident,
-        &args.generics,
+        args,
         quote! {{
             const VARIANTS: &[#ty_name] = &[#(#ty_name::#variant_idents,)*];
 
